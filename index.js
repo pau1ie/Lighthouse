a3af1160d5f75019f4a93d933933b72c023a1a05
Danny Liehr <dannyisyelling@gmail.com>
2021-12-11 21:14:46 -0500

    the app now has multiple pages.

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config();
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('public'))
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .use(function(){
+  //     express.static(__dirname + '/public');
+  //     session({
+  //     secret: process.env.sec,
+  //     resave: true,
+  //     saveUninitialized: true
+  //     });
+  // })
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+      var imgFolder= path.join(__dirname, 'public/img')
+      res.render('pages/index');
+      res.send(imgFolder);
+        }
+      )
+  .get('/test', (req, res) => res.render('pages/test'))
+  .get('/about', (req, res, next) => {
+      res.render('pages/about');
+      // res.locals.user = req.session.user;
+      // next();
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
